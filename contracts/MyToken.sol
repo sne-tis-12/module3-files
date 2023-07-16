1483746e75a42157798e6fb37cce62e4883dd363
sne-tis-12 <97742586+sne-tis-12@users.noreply.github.com>
2023-07-16 21:02:02 +0530

    Add files via upload

---
+++ contracts/MyToken.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract MyToken {
+    
+    string public tokenName = "mytoken";
+    string public tokenAbbvr = "MT";
+    uint public totalTokens = 0;
+    mapping(address => uint) balances;
+
+    address private owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function mint(address receiver, uint amount ) public {
+        require(msg.sender == owner, "Only owner can mint tokens");
+        balances[receiver] += amount;
+        totalTokens += amount;
+    }
+
+    function transfer(address receiver, uint amount) public {
+        address sender = msg.sender; 
+        uint balanceOfSender = balances[sender];
+        require(balanceOfSender >= amount, "Insufficient Funds");
+        balances[receiver] += amount;
+        balances[sender] -= amount;
+    }
+
+    function burn(uint amount) public {
+        address sender = msg.sender;
+        uint balanceOfSender = balances[sender];
+        require(balanceOfSender >= amount, "Insufficient Funds");
+        balances[sender] -= amount;
+        totalTokens -= amount;
+    }
+
+}
